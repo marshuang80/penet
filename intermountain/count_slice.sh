f2d8e331db7bff018f11c39e200ea2909db60346
marshuang80 <marshuang80@gmail.com>
2020-04-28 18:46:38 -0700

    [UPDATE] envrionment dependencies and requirements

--- intermountain/count_slice.sh
+++
@@ -1,1 +0,0 @@
-sudo python count_slice.py --input_dir /mnt/bbq/intermountain2/CTPA_Images/ 
--- run_test.sh
+++
@@ -1,1 +0,0 @@
-python ./test_intermountain.py --phase test --results_dir ../results --ckpt_path /mnt/bbq/PE_stanford/ckpts/best.pth.tar --name first_test --dataset pe --data_dir /mnt/bbq/PE_stanford/ --gpu_ids 0
